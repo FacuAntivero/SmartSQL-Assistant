6e7c681c37872144072cb4285b415d498afa2698
Facundo Antivero <73594978+FacuAntivero@users.noreply.github.com>
2026-04-30 11:14:35 -0300

    feat: change model of gemini

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -10,8 +10,7 @@ if (!process.env.GEMINI_API_KEY) {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-2.5-flash",
-    // 2. Le decimos a Gemini qué herramientas puede usar
+model: "gemini-1.5-flash", 
     tools: [{ functionDeclarations: barberTools.declarations }] 
 });
 
